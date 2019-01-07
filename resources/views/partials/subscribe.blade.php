a93591230a2c10c513c8ea94e9c5129249ddf0ba
Cindy Leschaud <cindyleschaud@droit-0248-br1.unine.ch>
2019-01-07 09:05:44 +0100

    fix typo inscription

--- resources/views/partials/subscribe.blade.php
+++ resources/views/partials/subscribe.blade.php
@@ -1,7 +1,7 @@
 <div class="widget">
     <h4><i class="icon-envelope"></i>&nbsp;Inscription à la newsletter</h4>
 
-    <a data-fancybox data-type="iframe" data-src="{{ url('https://shop.local/site/subscribe/'.env('APP_SITE')) }}" class="btn btn-default btn-block grey" href="javascript:;">
+    <a data-fancybox data-type="iframe" data-src="{{ url('https://publications-droit.ch/site/subscribe/'.env('APP_SITE')) }}" class="btn btn-default btn-block grey" href="javascript:;">
         Je m'inscris!
     </a>
 </div><!--END WIDGET-->
