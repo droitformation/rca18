64fa0244b83e4337df30c879140ef7f3b5bec022
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-05-05 09:05:12 +0200

    update laravel 7 add cache

--- app/Droit/Api/Content.php
+++ app/Droit/Api/Content.php
@@ -1,28 +1,32 @@
 <?php namespace App\Droit\Api;
 
+use App\Droit\Api\FetchData;
+
 class Content
 {
+    use FetchData;
+
     public $site;
+    public $file = 'content';
     protected $client;
     protected $base_url;
+    protected $helper;
 
-    public function __construct($site)
+    public $toUpdate = false;
+
+    public function __construct($site,$client = null)
     {
         $this->site = $site;
+        $this->helper = new \App\Droit\Helper\Helper();
+        $this->client = $client ? $client : new \GuzzleHttp\Client(['verify' => false, 'http_errors' => false]); // ,'debug' => true
 
-        $this->client  = new \GuzzleHttp\Client([ 'verify' => false ]);
+        $this->base_url = (\App::environment() == 'local' ? 'https://shop.test/hub' : 'https://www.publications-droit.ch/hub');
 
-        $environment = app()->environment();
-        $this->base_url = ($environment == 'local' ? 'https://shop.test/hub' : 'https://www.publications-droit.ch/hub');
+        $this->toUpdate();
     }
 
-    public function getData($url, $params = null){
-
-        $action = $params ? 'post' : 'get';
-
-        $response = $this->client->$action( $this->base_url.'/'.$url, ['query' => $params]);
-        $data     = json_decode($response->getBody(), true);
-
+    public function prepareData($data)
+    {
         if(!empty($data) && isset($data['data'])){
             return json_decode(json_encode($data['data']));
         }
@@ -35,7 +39,10 @@ class Content
         $params = ['params' => ['site_id' => $this->site]];
         $params = array_filter($params);
 
-        return $this->getData('homepage', $params);
+        return \Cache::rememberForever('homepage', function () use ($params) {
+            return $this->getData('homepage', $params);
+        });
+
     }
 
     public function page($id)
@@ -43,7 +50,9 @@ class Content
         $params = ['params' => ['site_id' => $this->site, 'id' => $id]];
         $params = array_filter($params);
 
-        return $this->getData('page', $params);
+        return \Cache::rememberForever('page_'.$id, function () use ($params,$id) {
+            return $this->getData('page', $params);
+        });
     }
 
     public function menu($position)
@@ -51,7 +60,9 @@ class Content
         $params = ['params' => ['site_id' => $this->site, 'position' => $position]];
         $params = array_filter($params);
 
-        return $this->getData('menu', $params);
+        return \Cache::rememberForever('menu_'.$position, function () use ($params) {
+            return $this->getData('menu', $params);
+        });
     }
 
 }
---
+++ app/Droit/Api/FetchData.php
@@ -0,0 +1,65 @@
+<?php namespace App\Droit\Api;
+
+trait FetchData
+{
+    public function toUpdate()
+    {
+        $current  = $this->helper->getMaj($this->file);
+        $response = $this->client->get($this->base_url.'/maj');
+
+        if($response->getStatusCode() == 503){
+            \Log::info('is down don\'t update');
+            return null;
+        }
+
+        $last = $this->process($response,$current);
+
+        if($last != $current){
+
+            \Log::info('last '.$last);
+            \Log::info('current '.$current);
+
+            \Log::info('flushed');
+            \Cache::flush();
+            $this->toUpdate = true;
+        }
+    }
+
+    public function process($response)
+    {
+        $last = \Carbon\Carbon::today()->toDateString();
+
+        // response from server
+        if($response->getStatusCode() == 200){
+
+            $data = json_decode($response->getBody(), true);
+            $last = isset($data['date']) ? $data['date'] : \Carbon\Carbon::today()->toDateString();
+
+            $this->helper->setMaj($last,$this->file);
+        }
+        else{
+            echo '<pre>';
+            print_r($response->getStatusCode());
+            echo '</pre>';
+            exit;
+        }
+
+        return $last;
+    }
+
+    public function getData($url, $params = null){
+
+        $action = $params ? 'post' : 'get';
+
+        try {
+            $response = $this->client->$action($this->base_url.'/'.$url, ['query' => $params]);
+            $data     = json_decode($response->getBody(), true);
+
+            return $this->prepareData($data);
+        }
+        catch (GuzzleException $error) {}
+
+        return collect([]);
+    }
+
+}
