1ff25b873d5335bd81f64d1c5862b1b95ae59ba0
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-10-20 13:20:14 +0200

    changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -87,6 +87,7 @@
 
             </div>
         </div><!-- END Container -->
+
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114403548-8"></script>
     <script>
