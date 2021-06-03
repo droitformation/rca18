d33385403221983155e43fc7e01df1c666a40870
Laurent Béguelin <laurent@noxup.ch>
2021-06-03 13:38:43 +0200

    Added deployer php

---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'rcassurances');
+
+// Project repository
+set('repository', 'git@github.com:droitformation/rca18.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Disable SSH multiplexing…
+set('ssh_multiplexing', false);
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+set('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+host('droitdutravail.ch')
+	->stage('prod')
+	->user('droitdut')
+	->port(22)
+	->addSshOption('UserKnownHostsFile', '/dev/null')
+	->addSshOption('StrictHostKeyChecking', 'no')
+    ->set('deploy_path', '~/deploy/{{application}}');    
+	
+// Hosts
+host('beg.noxup.ch')
+	->stage('dev')
+	->user('droitdut')
+	->port(22)
+	->addSshOption('UserKnownHostsFile', '/dev/null')
+	->addSshOption('StrictHostKeyChecking', 'no')
+    ->set('deploy_path', '~/dev/{{application}}');    
+
+// Tasks
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
